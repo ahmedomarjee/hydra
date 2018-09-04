29e39fc367c903f6829948cdbb9e95eecb14342c
Alex Silva <alex-silva@pluralsight.com>
2018-09-04 09:45:08 -0600

    Adds a degraded state to KafkaProducerProxy

--- kafka/src/main/scala/hydra/kafka/transport/KafkaProducerProxy.scala
+++ kafka/src/main/scala/hydra/kafka/transport/KafkaProducerProxy.scala
@@ -2,14 +2,17 @@ package hydra.kafka.transport
 
 import java.util.concurrent.TimeUnit
 
-import akka.actor.{Actor, Props, Stash}
+import akka.actor.{Actor, Cancellable, Props, Stash}
 import akka.kafka.ProducerSettings
 import hydra.common.logging.LoggingAdapter
 import hydra.core.protocol._
 import hydra.core.transport.TransportCallback
 import hydra.kafka.producer.{HydraKafkaCallback, KafkaRecord, KafkaRecordMetadata}
-import hydra.kafka.transport.KafkaProducerProxy.{ProduceToKafka, ProducerInitializationError}
+import hydra.kafka.transport.KafkaProducerProxy.{ProduceToKafka, ProducerInitializationError, ValidateKafkaConnection}
 import hydra.kafka.transport.KafkaTransport.RecordProduceError
+import hydra.kafka.util.KafkaUtils
+
+import scala.concurrent.duration._
 import org.apache.kafka.clients.producer.{Callback, KafkaProducer}
 
 import scala.util.{Failure, Success, Try}
@@ -28,8 +31,20 @@ class KafkaProducerProxy[K, V](id: String, settings: ProducerSettings[K, V])
 
   override def receive = initializing
 
-  def initializing: Receive = {
+  private def initializing: Receive = {
     case _ => stash()
+  }
+
+  private def degraded(task: Cancellable, ex: Throwable): Receive = {
+    case ValidateKafkaConnection =>
+      if (KafkaProducerProxy.validateKafkaConnection()) {
+        task.cancel()
+        context.unbecome()
+      }
+
+    case ProduceToKafka(deliveryId, kr, callback) =>
+      context.parent ! RecordProduceError(deliveryId, kr, ex)
+      callback.onCompletion(deliveryId, None, Some(ex))
   }
 
   private def producing: Receive = {
@@ -42,6 +57,9 @@ class KafkaProducerProxy[K, V](id: String, settings: ProducerSettings[K, V])
 
     case err: RecordProduceError =>
       context.parent ! err
+      val task = context.system.scheduler.schedule(5 seconds,
+        5 seconds, self, ValidateKafkaConnection)
+      context.become(degraded(task, err.error))
   }
 
   private def produce(r: KafkaRecord[K, V], callback: Callback) = {
@@ -92,15 +110,23 @@ class KafkaProducerProxy[K, V](id: String, settings: ProducerSettings[K, V])
 }
 
 
-object KafkaProducerProxy {
+object KafkaProducerProxy extends LoggingAdapter {
 
   case class ProduceToKafka(deliveryId: Long, kr: KafkaRecord[_, _],
                             callback: TransportCallback) extends HydraMessage
 
   case class ProducerInitializationError(id: String, ex: Throwable)
 
+  case object ValidateKafkaConnection
+
   def props[K, V](id: String, settings: ProducerSettings[K, V]): Props =
     Props(classOf[KafkaProducerProxy[K, V]], id, settings)
+
+  def validateKafkaConnection(): Boolean = {
+    val consumer = KafkaUtils.stringConsumerSettings.createKafkaConsumer()
+    Try(consumer.listTopics()).map(_ => true)
+      .recover { case e => log.error("KafkaProducerProxy", e); false }.get
+  }
 
 }
 
